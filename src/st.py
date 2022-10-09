19eea0d88e3cf4e7e318da5b10c533f033d0cd12
jojovin <93982083+jojovin@users.noreply.github.com>
2022-10-09 08:54:50 +0200

    make_suffix_tree code not quite working yet

--- src/st.py
+++ src/st.py
@@ -1,7 +1,5 @@
 import argparse
-from tree import
-
-
+from tree import Knæ
 def main():
     argparser = argparse.ArgumentParser(
         description="Exact matching using a suffix tree")
@@ -11,6 +9,42 @@ def main():
     print(f"Find every reads in {args.reads.name} " +
           f"in genome {args.genome.name}")
 
+def make_suffix_tree(x: str) -> None:
+    n = len(n)
+    root = Knæ(None, None, {})
+    current = root
+    for i, char in enumerate(x):
+        lam=0
+        j=0
+        while True:
+            # if x[i]=="$":
+            #     current.children=i
+            #     break
+            if lam==current.ben[1]-current.ben[0] or current.ben==None:
+                if x[i] in current.children and x[i]!="$":
+                    i+=1
+                    j+=1
+                    lam=0
+                    current=current.children[x[i]]
+                else:
+                    current.children[x[i]]=Knæ(current,(i,n-i-1),i)
+                    current=root
+                    break
+            if x[current.ben[0]+lam]==x[i]:
+                i+=1
+                j+=1
+                lam+=1
+            else:                
+                current.parent.children[x[current.ben[0]]]=Knæ(current.parent, (current.ben[0],current.ben[0]+lam-1),{x[current.ben[0]+lam]:current})
+                current.parent=current.parent.children[x[current.ben[0]]]
+                current.ben=(current.ben[0]+lam,current.ben[1])
+                current=current.parent
+                current.children[x[i]]=Knæ(current, (i,n-i-1),i-j)
+                current=root
+                break
+
+            
+make_suffix_tree("BBBABA")
 
 if __name__ == '__main__':
     main()
--- src/tree.py
+++ src/tree.py
@@ -17,5 +17,26 @@ class SuffixTree:
         self.root = Knæ(None, None, {})
         current = self.root
         for i, char in enumerate(x):
-            if char not in current.children:
-                current.children[char] = Knæ(current, (i, n), i)
+            if char in current.children:
+                lam=0
+                j=0
+                while True:
+                    if lam==current.ben[1]:
+                        lam=0
+                        if x[i+j] in current.children:
+                            current=current.children[x[i+j]]
+                        else:
+                            current.children[x[i+j]]=Knæ(current,(j),{})
+                    if x[current.ben[0]+lam]==x[i+j]:
+                        j+=1
+                        lam+=1
+                    else:
+
+                    
+
+            
+
+
+
+                    
+                            

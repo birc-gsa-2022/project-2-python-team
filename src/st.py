eb3f9e7e42f386c07b93fdcd85ff20f4d5336727
jojovin <jnv@birc.au.dk>
2022-10-10 10:59:55 +0200

    Fjernede nogle debug print-statements

--- src/st.py
+++ src/st.py
@@ -15,20 +15,17 @@ def make_suffix_tree(x: str) -> None:
     root = Knæ(None, (0,0), {})
     current = root
     for i, char in enumerate(x):
-        print(i)
         lam=0
         j=0
         while True:
             if lam==current.ben[1]-current.ben[0] and current.ben[1]!=n-1:
                 if x[i] in current.children and x[i]!="$":
                     current=current.children[x[i]]
-                    print("a1",i)
                     lam=0
                 else:
                     current.children[x[i]]=Knæ(current,(i,n-1),i)
                     while current.parent:
                         current=current.parent
-                    print("a",i)
                     break
             if x[current.ben[0]+lam]==x[i] and x[i]!="$":
                 i+=1
@@ -42,9 +39,7 @@ def make_suffix_tree(x: str) -> None:
                 current.children[x[i]]=Knæ(current, (i,n-1),i-j)
                 while current.parent:
                     current=current.parent
-                print("b",i)
                 break
-            print("c",i)
 
     return current
 
